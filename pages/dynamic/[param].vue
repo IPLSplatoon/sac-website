808d02cbe1e9ecf9597710df283209e47af4214f
inkfarer <inkfarer@inkfarer.com>
2022-01-02 12:25:09 +0200

    Add routes for testing

---
+++ pages/dynamic/[param].vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        entered param: {{ $route.params.param }}
+        <br>
+        <NuxtLink to="/" class="text-blue-500 underline">Back</NuxtLink>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,8 @@
   <div class="container mx-auto">
       <div class="bg-amber-300 px-8 text-center mt-8 py-4">
           <h1 class="font-bold text-3xl">hi :)</h1>
-          <p>i am a nuxt3 / tailwind example</p>
+          <p>i am a nuxt3 / tailwind test</p>
+          <NuxtLink to="/dynamic/test-param" class="text-blue-500 underline">Dynamic route</NuxtLink>
       </div>
   </div>
 </template>
