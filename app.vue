bbbd1980b3573da59e20fb927925762f402ef28d
inkfarer <inkfarer@inkfarer.com>
2022-01-02 12:08:29 +0200

    Add tailwind

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    hi :)
+  <div class="container mx-auto">
+      <div class="bg-amber-300 px-8 text-center mt-8 py-4">
+          <h1 class="font-bold text-3xl">hi :)</h1>
+          <p>i am a nuxt3 / tailwind example</p>
+      </div>
   </div>
 </template>
