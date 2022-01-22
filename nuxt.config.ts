57f5003610549ad6fbf60b106ff823e4df76ed44
inkfarer <inkfarer@inkfarer.com>
2022-01-22 15:39:26 +0200

    Tweak nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,5 @@
 import { defineNuxtConfig } from 'nuxt3'
 
-// Adding this configuration when using the dev server prevents CSS from loading.
-// Removing this configuration when in production mode prevents images from loading.
-const productionConfig = {
-    vite: {
-        base: '/'
-    }
-}
-
 export default defineNuxtConfig({
     css: ['@/assets/css/styles.scss'],
     build: {
@@ -27,6 +19,5 @@ export default defineNuxtConfig({
             { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
             { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }
         ]
-    },
-    ...(process.env.NODE_ENV === 'production' ? productionConfig : {})
+    }
 })
