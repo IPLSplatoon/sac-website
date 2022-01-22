0b8b83b73a8ec313b4f4bd623efd1e4645d82a6b
inkfarer <inkfarer@inkfarer.com>
2022-01-22 15:45:19 +0200

    Revert config changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 import { defineNuxtConfig } from 'nuxt3'
 
+// Adding this configuration when using the dev server prevents CSS from loading.
+// Removing this configuration when in production mode prevents images from loading.
+const productionConfig = {
+    vite: {
+        base: '/'
+    }
+}
+
 export default defineNuxtConfig({
     css: ['@/assets/css/styles.scss'],
     build: {
@@ -19,5 +27,6 @@ export default defineNuxtConfig({
             { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
             { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }
         ]
-    }
+    },
+    ...(process.env.NODE_ENV === 'production' ? productionConfig : {})
 })
