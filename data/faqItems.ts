5e99f923fd831ad5ccc9a705a00f21d52e76356c
ely <elymentalW@gmail.com>
2025-05-01 02:52:14 -0700

    add format to faq

--- data/faqItems.ts
+++ data/faqItems.ts
@@ -4,12 +4,16 @@ export default [
         description: 'Any player who is not LUTI Division 1 or above may play in SAC season 10.<br>While SAC is an event aimed for midlevel teams past the Low Ink skill cap, anyone under the SAC skill cap may play. Exception requests are handled in the IPL discord server if you have a banned player you think should be allowed in.'
     },
     {
-        title: 'When is the next tournament?',
-        description: 'A list of tournaments that are open for registration and prior from, for Season 10 is available to view on the home page.'
+        title: 'What is SAC’s format?',
+        description: 'SAC is a circuit spanning 4 qualifiers leading into a 1 day double elimination finals event. Qualifiers are swiss into gold/silver/bronze single elimination brackets and award points to teams relative to their placement. Winners of qualifiers automatically qualify for finals and the top 8 teams on the point standings qualify as well.'
     },
     {
         title: 'How does the point system work?',
         description: 'Points are awarded as follows: </p> <table class="table-auto border-collapse sac-table"><thead class="text-left bg-sac-blue-700 text-xl border-x-2 border-t-2 border-sac-blue-100"><tr><th>Placement &nbsp;</th><th>Points Awarded</th></tr></thead><tbody><tr><td>1st</td><td>150</td></tr><tr><td>2nd</td><td>130</td></tr><tr><td>3rd</td><td>120</td></tr><tr><td>4th</td><td>110</td></tr><tr><td>5-8th</td><td>100</td></tr><tr><td>9th</td><td>90</td></tr><tr><td>10th</td><td>75</td></tr><tr><td>11th</td><td>65</td></tr><tr><td>12th</td><td>60</td></tr><tr><td>13-16th</td><td>50</td></tr><tr><td>17th</td><td>40</td></tr><tr><td>18th</td><td>30</td></tr><tr><td>19th</td><td>25</td></tr><tr><td>20th</td><td>20</td></tr><tr><td>21st-24th</td><td>15</td></tr></tbody></table><p> With 1-8 is Gold Bracket, 9-16 being Silver bracket, and 17-24 being Bronze bracket. Teams that do not make a bracket will be given 2 points for every set win and 1 point for every set loss. If a team has the same amount of set wins as the 24th place team but does not advance to a bracket due to tiebreaks, their score for the tournament will be 10 points.'
+    },
+    {
+        title: 'When is the next tournament?',
+        description: 'A list of tournaments that are open for registration and prior from, for Season 10 is available to view on the home page.'
     },
     {
         title: 'Can I play with a pickup?',
